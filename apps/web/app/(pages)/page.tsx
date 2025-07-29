b79c11fddc81a33e2a1e744c7418032a247d9f27
Akshay-Vs <me.akshay.vs@gmail.com>
2025-07-29 20:37:35 +0530

    ✨ feat: add section block

--- apps/web/app/(pages)/page.tsx
+++ apps/web/app/(pages)/page.tsx
@@ -1,23 +1,16 @@
+import Block from '@/components/wrappers/block';
+
 export default function Home() {
   return (
     <div className="col-center gap-4  pt-4">
       <div className="w-full center gap-4 h-[45vh]">
-        <section className="w-[80%] h-[45vh] bg-surface-3-dark rounded-default p-4 border border-border-dark">
-          <p>Graph 1</p>
-        </section>
-
-        <section className="w-[20%] h-[45vh] bg-surface-3-dark rounded-default p-4 border border-border-dark">
-          <p>Info 1</p>
-        </section>
+        <Block className="w-[80%]">Graph 1 </Block>
+        <Block className="w-[20%]">Info</Block>
       </div>
-      <div className="w-full center gap-4 h-[45vh]">
-        <section className="w-1/2 h-[45vh] bg-surface-3-dark rounded-default p-4 border border-border-dark">
-          <p>Logs</p>
-        </section>
 
-        <section className="w-1/2 h-[45vh] bg-surface-3-dark rounded-default p-4 border border-border-dark">
-          <p>Graph 2</p>
-        </section>
+      <div className="w-full center gap-4 h-[45vh]">
+        <Block>Logs</Block>
+        <Block>Graph 2</Block>
       </div>
     </div>
   );

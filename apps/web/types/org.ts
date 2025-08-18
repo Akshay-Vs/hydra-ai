7e87241484e57df272c9c8a8422978b13f1073c1
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-18 21:13:06 +0530

    ✨ feat: add org type

--- apps/web/types/org.ts
+++ apps/web/types/org.ts
@@ -1,4 +1,10 @@
 export type Organization = {
   id: string;
   name: string;
+  avatar_url: string | null;
+  description: string | null;
+  website: string | null;
+  updated_at: string;
+  created_at: string;
+  deleted_at: string | null;
 };

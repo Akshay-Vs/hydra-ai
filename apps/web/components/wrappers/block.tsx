df0eb199d8f612d9290409d49b8c7034665d66b1
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-18 21:14:11 +0530

    ✨ feat: add click event to block wrapper

--- apps/web/components/wrappers/block.tsx
+++ apps/web/components/wrappers/block.tsx
@@ -1,9 +1,15 @@
+import type { MouseEvent } from 'react';
 import { cn } from '@hydra/ui/libs/utils';
 import type { BaseProps } from '@/types/baseprops';
 
-const Block = ({ className, children }: BaseProps) => {
+const Block = ({
+  className,
+  children,
+  onClick,
+}: BaseProps & { onClick: (e: MouseEvent<HTMLDivElement>) => void }) => {
   return (
     <section
+      onClick={onClick}
       className={cn(
         'w-1/2 h-[45vh] bg-surface-3-dark rounded-default p-4 border border-border-dark center relative',
         className

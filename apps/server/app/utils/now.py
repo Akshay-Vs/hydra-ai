ff280ff94e3b5f3723a6b7e299035eff1f064a5c
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-19 22:41:24 +0530

    ✨ feat: add now util to get current time

---
+++ apps/server/app/utils/now.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timezone
+
+
+def now():
+    """
+    Returns the current date and time in ISO 8601 format.
+    """
+
+    return datetime.now(timezone.utc).isoformat()

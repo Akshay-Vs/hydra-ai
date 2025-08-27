d9cf09f964898b9e075779c6ba66f0efd9fce79d
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-27 14:26:42 +0530

    ✨ feat: add Spinner component and integrate it into SplashScreen for loading indication

---
+++ apps/web/components/tokens/spinner.tsx
@@ -0,0 +1,7 @@
+import { Loader2 } from 'lucide-react';
+
+const Spinner = () => {
+  return <Loader2 className="animate-spin text-text-dark/50 duration-1000" />;
+};
+
+export default Spinner;
--- apps/web/components/tokens/splash-screen.tsx
+++ apps/web/components/tokens/splash-screen.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
 import Logo from '@/public/images/Hydra_Logo.svg';
+import Spinner from './spinner';
 
-const SplashScreen = () => {
+type Props = {
+  label?: string;
+  isLoading?: boolean;
+};
+
+const SplashScreen = ({ label, isLoading = true }: Props) => {
   return (
     <div className="absolute inset-0 top-0 left-0 col-center gap-6 bg-background-dark">
       <Image src={Logo} alt="Hydra Logo" className="w-32 h-32 mx-auto mt-20" />
-      <p className="text-text-dark/80">Initializing...</p>
+      <p className="text-text-dark/80">{label ?? 'Initializing...'}</p>
+      {isLoading ?? <Spinner />}{' '}
     </div>
   );
 };

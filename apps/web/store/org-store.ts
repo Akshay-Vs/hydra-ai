883add93fc35f83a43c841f7b29f6fff5a51a913
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-29 23:10:42 +0530

    🐛 fix: update OrgStore to use Organization type for selectedOrg and setSelectedOrg

--- apps/web/store/org-store.ts
+++ apps/web/store/org-store.ts
@@ -3,14 +3,18 @@ import type { Organization } from '@/types/org';
 
 interface OrgStore {
   orgs: Organization[];
-  selectedOrg: string | null;
+  selectedOrg: Organization | null;
+  selectedOrgId: string | null;
   setOrgs: (orgs: Organization[]) => void;
-  setSelectedOrg: (org: string) => void;
+  setSelectedOrg: (org: Organization) => void;
 }
 
 export const useOrgStore = create<OrgStore>(set => ({
   orgs: [],
   selectedOrg: null,
+  selectedOrgId: null,
   setOrgs: orgs => set({ orgs }),
-  setSelectedOrg: org => set({ selectedOrg: org }),
+  setSelectedOrg: org => {
+    set({ selectedOrg: org });
+  },
 }));

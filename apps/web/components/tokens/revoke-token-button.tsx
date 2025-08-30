284d0881378b85b4c88f3951c70143f35441d67e
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-30 14:53:55 +0530

    ✨ feat: implement optimistic updates for revoke token functionality

--- apps/web/components/tokens/revoke-token-button.tsx
+++ apps/web/components/tokens/revoke-token-button.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@hydra/ui/button';
+import { useQueryClient } from '@tanstack/react-query';
 import { Trash2 } from 'lucide-react';
 import { useMutate } from '@/hooks/use-mutate';
+import Spinner from './spinner';
 
 type Props = {
   credentialId: string;
@@ -8,7 +10,36 @@ type Props = {
 };
 
 const RevokeTokenButton = ({ credentialId, orgId }: Props) => {
-  const { mutateAsync, isPending, isSuccess } = useMutate();
+  const queryClient = useQueryClient();
+  const { mutateAsync, isPending } = useMutate<
+    void,
+    unknown,
+    { prevData: Credential[] | undefined }
+  >({
+    onMutate: async ({ url }) => {
+      const credentialId = url.split('/').pop();
+
+      await queryClient.cancelQueries({ queryKey: ['client_credentials'] });
+      const prevData = queryClient.getQueryData<Credential[]>(['client_credentials']);
+
+      queryClient.setQueryData<Credential[]>(['client_credentials'], old => {
+        return old?.filter(cred => cred.id !== credentialId) ?? [];
+      });
+
+      return { prevData };
+    },
+
+    onError: (_err, _variables, context) => {
+      if (context?.prevData) {
+        queryClient.setQueryData(['client_credentials'], context.prevData);
+      }
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['client_credentials'] });
+    },
+  });
+
   const handleRevoke = async () => {
     try {
       await mutateAsync({
@@ -21,8 +52,14 @@ const RevokeTokenButton = ({ credentialId, orgId }: Props) => {
   };
 
   return (
-    <Button className="w-14 center" disabled={isPending} onClick={handleRevoke}>
-      <Trash2 size={20} className="text-red-400" />
+    <Button
+      className="w-14 center"
+      disabled={isPending}
+      onClick={handleRevoke}
+      aria-label="Revoke Credential"
+      title="Revoke Credential"
+    >
+      {isPending ? <Spinner /> : <Trash2 size={20} className="text-red-400" />}
     </Button>
   );
 };

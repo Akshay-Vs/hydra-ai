fb433ff32bfbe62ac1a14d807fa531b87114e3f4
Akshay-Vs <me.akshay.vs@gmail.com>
2025-08-30 14:54:37 +0530

    ✨ feat: replace New Credential button with NewCredentialButton component

--- apps/web/components/tokens/credentials.tsx
+++ apps/web/components/tokens/credentials.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { Button } from '@hydra/ui/button';
 import { useFetch } from '@/hooks/use-fetch';
 import { useOrgStore } from '@/store/org-store';
 import { columns, type Credential } from '../data-tables/client-credentials/columns';
 import DataTable from '../data-tables/data-table';
 import Block from '../wrappers/block';
+import NewCredentialButton from './new-credentials-button';
 import Spinner from './spinner';
 
 const Credentials = () => {
@@ -18,9 +18,7 @@ const Credentials = () => {
     <Block className="full col min-h-[48dvh] !justify-start items-start">
       <div className="flex w-full justify-between items-center pb-4">
         <h1 className="px-1 text-2xl">Client Connection Credentials</h1>
-        <Button className="px-8 h-14 border-2 border-border-dark bg-surface-2-dark">
-          New Credential
-        </Button>
+        <NewCredentialButton />
       </div>
       <div className="full center">
         {isLoading ? (

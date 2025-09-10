b7f7500929573bb11639602083472809c46682e0
Akshay-Vs <me.akshay.vs@gmail.com>
2025-09-11 01:19:15 +0530

    ✨ feat: enhance TelemetryService with auto-aggregation & checkpoint management

--- apps/server/app/services/telemetry_service.py
+++ apps/server/app/services/telemetry_service.py
@@ -1,13 +1,19 @@
-from datetime import datetime
-import json
+from datetime import datetime, timezone
 from sqlmodel import Session
 
-from app.core.database.aggregate_reader import AggregateReader
 from app.core.database.aggregate_store import AggregateStore
+from app.core.database.aggregation_checkpoint import AggregationCheckpointStore
 from app.core.telemetry.aggregate_telemetry import AggregateTelemetry
 from app.core.types.aggregate_telemetry import AggregationGranularity
-from app.services.database_service import get_session
-from app.utils.datetime_from_now import datetime_from_now
+from app.models.sql_model import (
+    IncidentAggregation1h,
+    LogAggregation1m,
+    MetricAggregation1m,
+)
+from app.utils.logging import create_logger
+from app.utils.now import now
+
+logger = create_logger(__name__)
 
 
 class TelemetryService:
@@ -15,8 +21,9 @@ class TelemetryService:
         self.session = session
         self.aggregate_store = AggregateStore(session)
         self.aggrigator = AggregateTelemetry(session)
+        self.checkpoint_store = AggregationCheckpointStore(session)
 
-    def run_aggregation_job(
+    def run_all_aggregation_job(
         self,
         org_id: str,
         start_time: datetime,
@@ -27,97 +34,94 @@ class TelemetryService:
         Main aggregation job - typically run every minute via scheduler.
         Aggregates the previous minute's data.
         """
+        self.aggrigate_metrics(org_id, start_time, end_time, granularity)
+        self.aggrigate_logs(org_id, start_time, end_time, granularity)
+        self.aggrigate_incidents(org_id, start_time, end_time, granularity)
 
+    def aggrigate_metrics(
+        self,
+        org_id: str,
+        start_time: datetime,
+        end_time: datetime,
+        granularity: AggregationGranularity = AggregationGranularity.ONE_MINUTE,
+    ):
         metrics_args = self.aggrigator.aggregate_metrics(
             org_id, start_time, end_time, granularity
         )
-        logs_args = self.aggrigator.aggregate_logs(
-            org_id, start_time, end_time, granularity
-        )
-        incidents_args = self.aggrigator.aggregate_incidents(
-            org_id, start_time, end_time
-        )
 
-        # store aggregated data
+        # store aggregated metrics
         self.aggregate_store.store_metric_aggregations(
             org_id, metrics_args, granularity
         )
 
+    def aggrigate_logs(
+        self,
+        org_id: str,
+        start_time: datetime,
+        end_time: datetime,
+        granularity: AggregationGranularity = AggregationGranularity.ONE_MINUTE,
+    ):
+        logs_args = self.aggrigator.aggregate_logs(
+            org_id, start_time, end_time, granularity
+        )
+
+        # store aggregated logs
         self.aggregate_store.store_log_aggregations(org_id, logs_args, granularity)
 
+    def aggrigate_incidents(
+        self,
+        org_id: str,
+        start_time: datetime,
+        end_time: datetime,
+        granularity: AggregationGranularity = AggregationGranularity.ONE_MINUTE,
+    ):
+        incidents_args = self.aggrigator.aggregate_incidents(
+            org_id, start_time, end_time
+        )
+
+        # store aggregated incidents
         self.aggregate_store.store_incident_aggregations(
             org_id, incidents_args, granularity
         )
 
+    def auto_aggregate(self, org_id: str, threshold: int = (60 * 15)):  # runs every 15 minutes
+        """
+        Automatically aggregates the previous minute's data.
+        """
 
-if __name__ == "__main__":
-    with get_session() as session:
-        # telemetry_service = TelemetryService(session)
-        # telemetry_service.run_aggregation_job(
-        #     org_id="cmf17m0o6000107n3i3sumqup",
-        #     start_time=datetime_from_now(-90),
-        #     end_time=datetime.now(),
-        #     granularity=AggregationGranularity.ONE_MINUTE,
-        # )
-        #
-        getter = AggregateReader(session)
-        org_id = "cmf17m0o6000107n3i3sumqup"
+        try:
+            # Map each table to its respective aggregation method
+            aggregation_map = {
+                MetricAggregation1m: self.aggrigate_metrics,
+                LogAggregation1m: self.aggrigate_logs,
+                IncidentAggregation1h: self.aggrigate_incidents,
+            }
 
-        metrics = getter.get_aggregated_metrics(
-            org_id,
-            "system.cpu.usage_percent",
-            datetime_from_now(-90),
-            datetime.now(),
-            granularity=AggregationGranularity.ONE_MINUTE,
-        )
-        json_data = json.dumps(metrics, default=str, indent=2)
-        print("Metrics: ", json_data)
+            _now = now()
 
-        logs = getter.get_aggregated_logs(
-            org_id,
-            "my-fastapi-service",
-            datetime_from_now(-90),
-            datetime.now(),
-        )
+            for table, aggregator in aggregation_map.items():
+                last_aggregated_at = self.checkpoint_store.last_aggregated_at(
+                    org_id, table.__tablename__
+                )
 
-        json_data = json.dumps(
-            [log.model_dump() for log in logs], default=str, indent=2
-        )
-        print("Logs: ", json_data)
+                if last_aggregated_at.tzinfo is None:
+                    last_aggregated_at = last_aggregated_at.replace(tzinfo=timezone.utc)
 
-        metrics_summery = getter.get_service_metrics_summary(
-            org_id,
-            "my-fastapi-service",
-            datetime_from_now(-90),
-            datetime.now(),
-        )
-        json_data = json.dumps(metrics_summery, default=str, indent=2)
-        print("Metrics summary: ", json_data)
+                time_delta = _now - last_aggregated_at
 
-        top_errors = getter.get_top_error_services(
-            org_id,
-            start_time=datetime_from_now(-90),
-            end_time=datetime.now(),
-            limit=10,
-        )
-        json_data = json.dumps(top_errors, default=str, indent=2)
-        print("List of top errors: ", json_data)
+                logger.debug(f"Last aggregated at: {last_aggregated_at}")
+                logger.debug(f"Now: {_now}")
+                logger.debug(f"Threshold: {threshold}")
+                logger.debug(f"Time delta: {time_delta}")
 
-        trends = getter.get_service_incident_trends(
-            org_id,
-            "my-fastapi-service",
-            start_time=datetime_from_now(-90),
-            end_time=datetime.now(),
-        )
-        json_data = json.dumps(trends, default=str, indent=2)
-        print(json_data)
+                if time_delta.total_seconds() > threshold and aggregator:
+                    logger.info(
+                        f"Auto-aggregated {table.__tablename__} data for {org_id} from {last_aggregated_at} to {_now}"
+                    )
+                    aggregator(org_id, last_aggregated_at, _now)
+                    self.checkpoint_store.update_timestamp(
+                        org_id, table.__tablename__, _now
+                    )
 
-        anomalies = getter.get_anomaly_candidates(
-            org_id,
-            "my-fastapi-service",
-            start_time=datetime_from_now(-90),
-            end_time=datetime.now(),
-            threshold_multiplier=3.0,
-        )
-        json_data = json.dumps(anomalies, default=str, indent=2)
-        print(json_data)
+        except Exception as e:
+            logger.error(f"Error in auto-aggregation: {e}")
